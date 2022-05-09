e42b86a5627b2420dd22706841b7897f0d8424bb
klina8575 <klina8575@gmail.com>
2022-05-09 18:30:57 +0900

    up

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,29 +3,43 @@ import React from "react";
 function TodoItem({ todo, onToggle, onRemove }) {
   return (
     <div>
-      <input type="checkbox" />
-      <span>예제 텍스트</span>
-      <button>삭제</button>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
     </div>
   );
 }
 
-function Todos({ input, todos, onChangeInput, OnInsert, onToggle, onRemove }) {
+function Todos({ input, todos, onChangeInput, onInsert, onToggle, onRemove }) {
   const onSubmit = (e) => {
     e.preventDefault();
+    onInsert(input);
+    onChangeInput(""); //등록 후 인풋 초기화
   };
+
+  const onChange = (e) => onChangeInput(e.target.value);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input />
+        <input value={input} onChange={onChange} />
         <button type="submit">등록</button>
       </form>
       <div>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
       </div>
     </div>
   );
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -9,21 +9,27 @@ const CounterContainer = ({ number, increase, decrease }) => {
 };
 
 //리덕스 스토어 안의 state를 컴포넌트(CounterContainer)의 props로 넘겨주기 위한 함수
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
 
-//액션 생성 함수를 컴포넌트(CounterContainer)의 props로 넘겨주기 위한 함수
-const mapDispatchToProps = (dispatch) => ({
-  increase,
-  decrease,
-  //   increase: () => {
-  //     dispatch(increase());
-  //   },
-  //   decrease: () => {
-  //     dispatch(decrease());
-  //   },
-});
+// //액션 생성 함수를 컴포넌트(CounterContainer)의 props로 넘겨주기 위한 함수
+// const mapDispatchToProps = (dispatch) => ({
+//     increase: () => {
+//       dispatch(increase());
+//     },
+//     decrease: () => {
+//       dispatch(decrease());
+//     },
+// });
 
 //connect: 컴포넌트를 리덕스와 연결
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  }
+)(CounterContainer);
