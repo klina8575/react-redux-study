d2f4e52484ec33fce44bff63e8dd73a391e92fc1
klina8575 <klina8575@gmail.com>
2022-05-10 14:40:59 +0900

    up

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,30 +1,44 @@
+import { createAction, handleActions } from "redux-actions";
+
 //액션 타입: '모듈이름/액션이름' 으로 명명
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 //액션 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //초기상태, 리듀서(데이터 변형)
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  //리듀서는 state와 action을 받음
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   //리듀서는 state와 action을 받음
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// handleActions(각 액션에 대한 업데이트 함수, 초기 state)
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
 
 export default counter;
