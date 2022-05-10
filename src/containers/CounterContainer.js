204da0a7bfea913988cefe23c055f4ed949a33ed
klina8575 <klina8575@gmail.com>
2022-05-10 15:21:15 +0900

    up

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,35 +1,17 @@
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import React, { useCallback } from "react";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
-//리덕스 스토어 안의 state를 컴포넌트(CounterContainer)의 props로 넘겨주기 위한 함수
-// const mapStateToProps = (state) => ({
-//   number: state.counter.number,
-// });
-
-// //액션 생성 함수를 컴포넌트(CounterContainer)의 props로 넘겨주기 위한 함수
-// const mapDispatchToProps = (dispatch) => ({
-//     increase: () => {
-//       dispatch(increase());
-//     },
-//     decrease: () => {
-//       dispatch(decrease());
-//     },
-// });
-
 //connect: 컴포넌트를 리덕스와 연결
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+export default React.memo(CounterContainer);
